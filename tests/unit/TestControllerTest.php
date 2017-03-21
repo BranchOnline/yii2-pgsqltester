f56872ca33d4141d27e9d588e053e128621a09a0
Roelof Ruis <roelof@branchonline.nl>
2017-03-21 19:41:10 +0100

    Updated test controller test

--- tests/unit/TestControllerTest.php
+++ tests/unit/TestControllerTest.php
@@ -6,31 +6,37 @@ use Codeception\Test\Unit;
 class TestControllerTest extends Unit {
 
     public function testDefaults() {
-        $test_controller = new TestController('test', Yii::$app);
+        $test_controller = $this->setupController();
         $this->assertSame('testing_template', $test_controller->testing_template_db);
         $this->assertSame('testing', $test_controller->testing_db);
         $this->assertSame('@console/migrations/', $test_controller->migration_path);
-        $this->assertSame('unit', $test_controller->suite);
+        $this->assertSame('', $test_controller->suite);
         $this->assertSame('', $test_controller->for_module);
+        $this->assertSame(false, $test_controller->coverage);
+        $this->assertSame(false, $test_controller->silent);
     }
 
     public function testOptionsAvailable() {
-        $test_controller = new TestController('test', Yii::$app);
-        $this->assertSame(['suite', 'for_module'], $test_controller->options('run'));
+        $test_controller = $this->setupController();
+        $this->assertSame(['suite', 'for_module', 'coverage', 'silent'], $test_controller->options('run'));
     }
 
     public function testConfigDbInvalid() {
         Yii::$app->set('config_db', null);
         $this->expectException('yii\base\InvalidConfigException');
         $this->expectExceptionMessage('No database component configured for \'config_db\'.');
-        new TestController('test', Yii::$app);
+        $this->setupController();
     }
 
     public function testDbInvalid() {
         Yii::$app->set('db', null);
         $this->expectException('yii\base\InvalidConfigException');
         $this->expectExceptionMessage('No database component configured for \'db\'.');
-        new TestController('test', Yii::$app);
+        $this->setupController();
+    }
+
+    protected function setupController() {
+        return new TestController('test', Yii::$app);
     }
 
 }
